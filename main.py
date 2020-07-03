41f1146fbb73aeb9707587aebd52d1ce0f38d1ed
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-07-03 14:17:54 -0400

    updating repo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Objectives:
 try:
     from cvelookup.db.functions import *
     from cvelookup.db.db_lookup import Database
-    from cvelookup.src.colors import Color as col
+    from colors import Color as col
     import sqlite3
     import subprocess
     import sys
@@ -19,22 +19,6 @@ try:
     import readline
 except ImportError as err:
     print(err)
-
-def get_help():
-    """Print help message"""
-    print(
-    """
-    Command  Arguments             Description
-    ------   ---------             -----------
-    help     None                 : display this help message
-    showcve  None                 : show all CVE id's
-    showall  None                 : show CVE id's and descriptions
-    search   [string]             : search CVE database for specific string
-    year     (>|<|=|>=|<=) [year] : get CVE's for a particular year or range of years
-    exit     None                 : exit the program
-    cls      None                 : clear screen
-    """
-    )
 
 class CveLookup:
     def get_help():
@@ -71,8 +55,8 @@ class CveLookup:
 
     def prompt(self):
         """Displays prompt and handles all commands"""
-        while 1:
-            user_input = input(col.m+"cvelookup"+col.re+"> ").strip()
+        while True:
+            user_input = input(col.m+"cvelookup"+col.re+" > ").strip()
             if user_input in self.command_dict.keys():
                 self.command_dict[user_input]()
 
