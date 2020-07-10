58832442f72fe09632952b548269e16e8f222e15
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-07-09 21:55:33 -0400

    Add files via upload

--- cvelookup/db/dblookup.py
+++ cvelookup/db/dblookup.py
@@ -5,34 +5,35 @@ Fetch and return data from the CVE database
 import sqlite3
 from cvelookup.db.dbutils import Dbutils
 from colored import fg, attr
+import re
 
 class Database(Dbutils):
     def search(self, args):
         """Query the database for a particular string"""
         conn = super().establish_connection()
         cursor = conn.cursor()
-        # This SQL query is vulnerable to sql injection
-        # but because this data is public there is no need to worry!
-        # It was used because the sqlite3 parameterized queries was
-        # not doing what I wanted it to do! :(
+
         sqlqry = f'''
             SELECT cve_id, description
             FROM CVE
             WHERE cve_id LIKE '%{args[0]}%' OR description LIKE '%{args[0]}%';
         '''
         results = cursor.execute(sqlqry)
+
         search_str_len = len(args[0])
         for row in results:
             if args[0] in row[0]:
                 search_start_pos = row[0].find(args[0])
-                print(row[0][:search_start_pos].ljust(14, " "), ("%s"+row[0][search_start_pos:search_start_pos+search_str_len]+"%s") % (fg(9), attr(0))+
-                    row[0][search_start_pos+search_str_len:])
+                print(("%s"+row[0][:search_start_pos].ljust(14, " ")+"%s") % (fg(9),
+                attr(0))+("%s"+row[0][search_start_pos:search_start_pos+search_str_len]+"%s") % (fg(49),
+                attr(0))+row[0][search_start_pos+search_str_len:])
             else:
-                print(row[0].ljust(14, " "), end=" ")
+                print(("%s"+row[0].ljust(14, " ")+"%s") % (fg(9), attr(0)), end=" ")
             if args[0] in row[1]:
                 search_start_pos = row[1].find(args[0])
-                print("|", row[1][:search_start_pos]+("%s"+row[1][search_start_pos:search_start_pos+search_str_len]+"%s") % (fg(9), attr(0))+
-                    row[1][search_start_pos+search_str_len:])
+                print("|", row[1][:search_start_pos],
+                ("%s"+row[1][search_start_pos:search_start_pos+search_str_len]+"%s") % (fg(49),
+                attr(0))+row[1][search_start_pos+search_str_len:])
             else:
                 print(row[1])
         conn.close()
@@ -41,37 +42,16 @@ class Database(Dbutils):
         """Fetches cve's from a particular year or a range of years"""
         conn = super().establish_connection()
         cursor = conn.cursor()
-        # This SQL query is vulnerable to sql injection
-        # but because this data is public there is no need to worry!
-        # It was used because the sqlite3 parameterized queries was
-        # not doing what I wanted it to do! :(
-        sqlqry = '''
+
+        sqlqry = f'''
             SELECT cve_id, description
             FROM CVE
-            WHERE CAST(SUBSTR(cve_id, 5, 4) AS INT) %s %s;
-        ''' % (args[0], args[1])
-        results = cursor.execute(sqlqry)
-        for row in results:
-            print(("%s"+row[0]+"%s") % (fg(9), attr(0)), "|", row[1][1:])
-        conn.close()
-
-    def show_cve(self):
-        """Retrieve all CVE id's from CVE database"""
-        conn = super().establish_connection()
-        cursor = conn.cursor()
-        sqlqry = '''
-            SELECT cve_id
-            FROM CVE;
+            WHERE CAST(SUBSTR(cve_id, 5, 4) AS INT) {args[0]} {args[1]};
         '''
         results = cursor.execute(sqlqry)
-        i = 0
+
         for row in results:
-            current_year = row[0][3:7]
-            if i % 15 == 0:
-                print("\n")
-            print(row[0], end=" ")
-            i += 1
-        print("\n")
+            print(("%s"+row[0]+"%s") % (fg(9), attr(0)), "|", row[1][1:])
         conn.close()
 
     def show_all(self):
@@ -87,4 +67,3 @@ class Database(Dbutils):
             print(("%s"+str(row[0])+"%s") % (fg(220), attr(0)), "|", ("%s"+row[1]+"%s") % (fg(196), attr(0)), "|", row[2][1:])
         conn.close()
 
-# 22 - first the columns
